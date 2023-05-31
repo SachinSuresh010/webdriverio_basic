11ef2ab57cc659d0c61b2a09d81ab7f67358c8ac
jessengrgspringer <jessengrg+github@gmail.com>
2023-05-31 15:51:21 +0530

    config change revert

--- wdio.conf.js
+++ wdio.conf.js
@@ -21,7 +21,7 @@ exports.config = {
     // will be called from there.
     //
     specs: [
-      './test/**/specs/**/*.spec.js'
+        './test/specs/**/*.js'
     ],
     // Patterns to exclude.
     exclude: [
