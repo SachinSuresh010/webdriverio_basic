2bded76179556f66923c47a9422516f0f9d1b3d0
archana mohan <archana.mohan24@gmail.com>
2023-05-31 13:08:37 +0530

    opening edDelivery login pop up

--- test/archana/specs/test.spec.js
+++ test/archana/specs/test.spec.js
@@ -1,6 +1,16 @@
-describe('test 31/05/2023',()=>{
-    it('first step',async()=>{
+const {loginPage}= require('../page-objects/test-file.js');
 
-    });
+describe('Basic folw of edDelivery',()=>{
     
-});
+    it('open url and load the application', async()=>{
+        await browser.maximizeWindow();
+        await loginPage.openUrl('https://edelivery.zoproduct.com/');
+        await expect(browser).toHaveUrl("https://edelivery.zoproduct.com/");
+    });
+
+    it('login to the application',async()=>{
+      await loginPage.openLoginPopUp();
+      expect(await loginPage.$loginPopUpWondow()).toBeDisplayed(10000);
+    });
+
+});   
