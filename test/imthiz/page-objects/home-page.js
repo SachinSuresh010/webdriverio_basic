588048fe922731e0606d246df02341631448d886
imdhiz <imdhiz@gmail.com>
2023-06-01 18:00:36 +0530

    flipkart flow initial commit

--- test/imthiz/page-objects/home-page.js
+++ test/imthiz/page-objects/home-page.js
@@ -1,5 +1,14 @@
+const { searchResultsPage } = require("./search-results-page.js");
+
 class HomePage {
-  constructor() {}
+  constructor() {
+    this.$loginButton = () =>
+      $('//div[@class="_1psGvi _3BvnxG"]//a[text()="Login"]');
+    this.$searchBox = () => $('//input[@class="_3704LK"]');
+    this.$searchSubmitButton = () => $('//button[@type="submit"]');
+    this.$initialLoginPopUpCloseButton = () =>
+      $('//button[@class="_2KpZ6l _2doB4z"]');
+  }
 
   /**
    * Load the url for the site and open the website
@@ -8,6 +17,32 @@ class HomePage {
   async openUrl(url) {
     await browser.maximizeWindow();
     await browser.url(url);
+    await this.$initialLoginPopUpCloseButton().waitForDisplayed({
+      timeout: 5000,
+      timeoutMsg: "wait time out failed for loading URL",
+    });
+  }
+  /**
+   * Method to close the pop up window for login when entering the URL
+   */
+  async loginPopUpClosing() {
+    await this.$initialLoginPopUpCloseButton().click();
+    await this.$loginButton().waitForDisplayed({
+      timeout: 5000,
+      timeoutMsg: "wait time out failed for loading URL",
+    });
+  }
+  /**
+   * Method to search certain data in search box  and load the result  page
+   * @param {String} searchData
+   */
+  async searchInput(searchData) {
+    await this.$searchBox().setValue(searchData);
+    await this.$searchSubmitButton().click();
+    await searchResultsPage.$searchResultIdentifier().waitForDisplayed({
+      timeout: 5000,
+      timeoutMsg: "wait time out failed for search results page to load",
+    });
   }
 }
 
